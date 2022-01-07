c4ba161fcb8d4464c35842387e287dc964bcbd46
MalachiU410 <urquhartmalachi@gmail.com>
2022-01-07 10:58:51 -0800

    Moved Chapter 1 code
    Moved chapter 1 code from a temporary folder over to github

---
+++ Chapter 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Program1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name, please:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
